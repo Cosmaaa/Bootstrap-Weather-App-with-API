57180e03b15544fd7ea017c56c557e7c6be865b5
Cosmaaa <cosma.andrei2001@yahoo.com>
2023-04-27 01:25:24 -0700

    some changes

--- App.js
+++ App.js
@@ -10,12 +10,35 @@ const spitOutCelsius=(kelvin)=>{
 
 updateWeatherApp=(city)=>{
     console.log(city);
+    const imageName=city.weather[0].icon;
+    const iconSrc=''
     cityName.textContent=city.name;
-    cardBody.innerHTML='<div class="card-mid row"><div class="col-8 text-center temp"><span>30&deg;C</span></div><div class="col-4 condition-temp"><p class="condition">Thunder Storm</p><p>30&deg;C</p></div></div><div class="icon-container card shadow"><img src="img/1489136024.svg" alt=""></div><div class="card-bottom px-5 py-4 row"><div class="col text-center1"><p>30&deg;C</p><span>Feels like</span></div><div class="col text-center2"><p>55%</p><span>Humidity</span></div></div>';
-    
+    cardBody.innerHTML=` <div class="card-mid row">
+    <div class="col-8 text-center temp">
+        <span>30&deg;C</span>
+    </div>
+    <div class="col-4 condition-temp">
+        <p class="condition">${city.weather[0]}</p>
+        <p class="temperature">${city.name.temp_max}&deg;C</p>
+    </div>
+</div>
+<div class="icon-container card shadow">
+    <img src="img/1489136024.svg" alt="">
+</div>
+<div class="card-bottom px-5 py-4 row">               
+    <div class="col text-center1">
+        <p>${city.name.temp_max}&deg;C</p>
+        <span>Feels like</span>
+    </div>
+    <div class="col text-center2">
+        <p>55%</p>
+        <span>Humidity</span>
+    </div>
+</div>`;
+   
 }
 
-                                                    //adding an event listener to the form
+                                                    //form event listenerrr
 searchForm.addEventListener('submit',event=>{
 
     event.preventDefault();
